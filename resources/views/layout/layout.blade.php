2fed7f8a41e313761613a92c52cd983e3d18d9b3
fildahananda@gmail.com <fildahananda@gmail.com>
2024-07-10 12:51:28 +0700

    first commit

---
+++ resources/views/layout/layout.blade.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html>
+<head>
+    @include('layout.partials.head')
+</head>
+<body>
+@include('layout.partials.sidenav')
+<!-- Main content -->
+<div class="main-content" id="panel">
+    @include('layout.partials.nav')
+    @include('layout.partials.header')
+    <div class="container-fluid mt--6">
+        @yield('content')
+        @include('layout.partials.footer')
+    </div>
+</div>
+@include('layout.partials.footer-scripts')
+</body>
+</html>
---
+++ resources/views/layout/partials/header.blade.php
@@ -0,0 +1,25 @@
+<!-- Header -->
+<div class="header bg-orange pb-6">
+    <div class="container-fluid">
+        <div class="header-body">
+            <div class="row align-items-center py-4">
+                <div class="col-lg-6 col-7">
+                    <h4 class="h2 text-white d-inline-block mb-0">@yield('pagetitle')</h4>
+{{--                    <nav aria-label="breadcrumb" class="d-none d-md-inline-block ml-md-4">--}}
+{{--                        <ol class="breadcrumb breadcrumb-links breadcrumb-dark">--}}
+{{--                            <li class="breadcrumb-item"><a href="#"><i class="fas fa-home text-orange"></i></a></li>--}}
+{{--                            <li class="breadcrumb-item"><a href="#" class="text-orange">Dashboards</a></li>--}}
+{{--                            <li class="breadcrumb-item active" aria-current="page">Default</li>--}}
+{{--                        </ol>--}}
+{{--                    </nav>--}}
+                </div>
+                <div class="col-lg-6 col-5 text-right">
+                    <a href="#" class="btn btn-sm btn-neutral">New</a>
+                    <a href="#" class="btn btn-sm btn-neutral">Filters</a>
+                </div>
+            </div>
+            <!-- Card stats -->
+
+        </div>
+    </div>
+</div>
